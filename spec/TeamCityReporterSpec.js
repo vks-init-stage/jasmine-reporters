9130dcf260a4ad744dc11908ba91652b270cd9c2
palashmarantas <91466368+palashmarantas@users.noreply.github.com>
2022-02-10 20:31:40 +0530

    Create TeamCityReporterSpec.js

--- spec/TeamCityReporterSpec.js
+++ spec/TeamCityReporterSpec.js
@@ -1,4 +1,4 @@
-/* globals jasmine, describe, beforeEach, it, expect */
+/* globals jasmine, describe, beforeEach, it, expect sdfsdf*/
 var jasmineReporters = require("../index");
 
 var env, suite, subSuite, subSubSuite,
